6f8e72c5760de5389334df5929b90a9d387bc18c
Tanzeel-Azeem <azeemtanzeel39@gmail.com>
2024-12-26 09:50:31 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let inputValue1 = + prompt('Enter your first value')
+let inputValue2 = + prompt('Enter your second value')
+
+let add = inputValue1 + inputValue2
+let sub = inputValue1 - inputValue2
+let mul = inputValue1 * inputValue2
+let div = inputValue1 / inputValue2
+let mod = inputValue1 % inputValue2
+
+alert(      'Addition: '+ add +    
+     '\n' + 'Subtraction: ' + sub + 
+     '\n' + 'Multiplication: ' + mul + 
+     '\n' + 'Division: ' + div + 
+     '\n' + 'Modulus: ' + mod)
