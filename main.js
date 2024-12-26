c11cd684bc4602e31ab6fd7f5524bf24bb1901e0
Tanzeel-Azeem <azeemtanzeel39@gmail.com>
2024-12-26 10:08:23 +0500

    adds conditions

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let inputValue1 = + prompt('Enter your first value')
 let inputValue2 = + prompt('Enter your second value')
+let operator = + prompt('which operation do you want to perform? \n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division \n 5. Modulus')
 
 let add = inputValue1 + inputValue2
 let sub = inputValue1 - inputValue2
@@ -7,8 +8,22 @@ let mul = inputValue1 * inputValue2
 let div = inputValue1 / inputValue2
 let mod = inputValue1 % inputValue2
 
-alert(      'Addition: '+ add +    
-     '\n' + 'Subtraction: ' + sub + 
-     '\n' + 'Multiplication: ' + mul + 
-     '\n' + 'Division: ' + div + 
-     '\n' + 'Modulus: ' + mod)
+
+    if (operator === 1 ){
+        alert ('ADDITION: ' + add)
+    }
+    else if (operator === 2){
+        alert ('SUBTRACTION: ' + sub)
+    }
+    else if (   operator === 3){
+        alert ('MULTIPLICATION: ' + mul)
+    }
+    else if (operator === 4){
+        alert ('DIVISION: ' + div)
+    }
+    else if (operator === 5){
+        alert ('MODULUS: ' + mod)
+    }
+    else {
+        alert ('Invalid Input')
+    }
